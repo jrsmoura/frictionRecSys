eb1b926df04f63f94f8f37a7fb69f7b0ad915c77
JR Steiner <jrsteiner78@gmail.com>
2023-04-25 17:38:15 -0300

    add pre_processing pipeline

--- src/frictionrecsys/pipelines/preprocessing/nodes.py
+++ src/frictionrecsys/pipelines/preprocessing/nodes.py
@@ -2,3 +2,7 @@
 This is a boilerplate pipeline 'preprocessing'
 generated using Kedro 0.18.7
 """
+import pandas as pd
+
+def pre_processing(data:pd.DataFrame) -> pd.DataFrame:
+    return data
--- src/frictionrecsys/pipelines/preprocessing/pipeline.py
+++ src/frictionrecsys/pipelines/preprocessing/pipeline.py
@@ -4,7 +4,15 @@ generated using Kedro 0.18.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import pre_processing
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=pre_processing,
+            inputs="base_ano",
+            outputs="preprocessed_base_ano",
+            name="preprocess_ano_base_node"
+        )
+    ])
